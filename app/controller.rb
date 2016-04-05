758e770e5b799094c08f7e4bbae7e5df33190669
Gabrielle <gabivk@gmail.com>
2016-04-05 14:31:30 +0100

    added controller

--- app/controller.rb
+++ app/controller.rb
@@ -2,9 +2,16 @@ require 'sinatra/base'
 
 class SpaceBnB < Sinatra::Base
   get '/' do
-    send_file 'public/index.html'
+    send_file 'public/user/new.html'
   end
 
+  post '/register' do
+    redirect '/welcome'
+  end
+
+  get '/welcome' do
+    send_file 'public/user/welcome.html'
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- app/spec/features/sign_up_spec.rb
+++ app/spec/features/sign_up_spec.rb
@@ -1,7 +1,19 @@
+require_relative '../helpers'
 feature 'Sign Up' do
+  doubles
+
   scenario 'directs user to sign up page' do
     visit '/'
-    click_button 'Sign Up'
     expect(page).to have_content('Welcome! Fill in your details below.')
+  end
+
+  scenario 'requires user details' do
+    visit '/'
+    fill_in :name, with: name
+    fill_in :username, with: username
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_button 'Submit'
+    expect(page).to have_content('Thanks, you have been signed up!')
   end
 end
