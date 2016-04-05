7c2f992a18a99cbfaddcf442681ba5df6a609e26
Misa Ogura <misa.ogura01@gmail.com>
2016-04-05 14:30:13 +0100

    Creating interface.js for feature testing the list page

--- app/controller.rb
+++ app/controller.rb
@@ -5,8 +5,19 @@ class SpaceBnB < Sinatra::Base
     'Hello SpaceBnB!'
   end
 
-  get '/space/new' do
-    redirect 'space_new.html'
+  get '/spaces/new' do
+    redirect 'spaces_new.html'
+  end
+
+  post '/spaces' do
+    Space.create(name: params[:name],
+                 description: params[:description],
+                 price: params[:price])
+    redirect '/spaces/list'
+  end
+
+  get '/spaces/list' do
+    redirect 'spaces_list.html'
   end
 
   # start the server if ruby file executed directly
--- app/spec/features/adding_new_space_spec.rb
+++ app/spec/features/adding_new_space_spec.rb
@@ -1,9 +1,20 @@
 feature "Adding new space" do
   scenario "User can see name, description and price forms on page" do
-    visit '/space/new'
+    visit '/spaces/new'
     expect(page).to have_field("name")
     expect(page).to have_field("description")
     expect(page).to have_field("price")
     expect(page).to have_button("List this space")
+  end
+
+  scenario "User can add a new space with name/desc/price" do
+    visit '/spaces/new'
+    fill_in(:name, with: "property1")
+    fill_in(:description, with: "one bedroom flat")
+    fill_in(:price, with: 75)
+    expect{ click_button("List this space") }.to change(Space, :count).by(1)
+    expect(page).to have_content("Name: property1")
+    expect(page).to have_content("Description: one bedroom flat")
+    expect(page).to have_content("Price per night: £75")
   end
 end
