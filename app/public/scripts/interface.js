7bca1f142ecc5c31916fd5528ba30ba07bf22732
Gabrielle <gabivk@gmail.com>
2016-04-06 15:58:21 +0100

    added Moment Library, created ranges!!!!!!!!!

--- app/public/scripts/interface.js
+++ app/public/scripts/interface.js
@@ -7,9 +7,8 @@ $( document ).ready(function() {
       $('#name').text('Name: ' + data.name);
       $('#description').text('Description: ' + data.description);
       $('#price').text('Price per night: £' + data.price);
-      // var dates = dateRange(data);
-      // console.log(dates);
-      $('#dates').text('Available dates: ' + data.date[0] + ' to ' + data.date[1] );
+      var dates = dateRange(data);
+      $('#dates').text('Available dates: ' +  dates );
     });
   }
 
@@ -18,15 +17,12 @@ $( document ).ready(function() {
   function dateRange(data){
     var startDate = new Date(data.date[0]);
     var endDate   = new Date(data.date[1]);
-    var dateRange = [startDate];
-    var currentDate = startDate;
-    while (startDate <= endDate)
-      console.log(currentDate);
-      currentDate.setDate(currentDate.getDate() + 1);
-      console.log(currentDate);
-      dateRange.push(currentDate);
-      console.log(dateRange);
-
+    var dateRange = [];
+    while (startDate <= endDate){
+      dateRange.push(moment(startDate).format(" MMM Do 'YY"));
+      startDate.setDate(startDate.getDate() + 1);
+    }
     return dateRange;
   }
+
 });
