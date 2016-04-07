843807c0df9c2ff8d8657f62aac88a48df282fdc
Misa Ogura <misa.ogura01@gmail.com>
2016-04-07 10:01:20 +0100

    Add current_user helper method in controleer

--- app/controller.rb
+++ app/controller.rb
@@ -74,6 +74,12 @@ class SpaceBnB < Sinatra::Base
     redirect '/spaces/list'
   end
 
+  helpers do
+    def current_user
+      current_user ||= User.get(session[:user_id])
+    end
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
