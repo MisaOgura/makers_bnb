65836135bebc1ef3dfcbb507fd0b1b9a5294be18
Joseph Sharratt <joseph.sharratt@gmail.com>
2016-04-09 12:41:01 +0100

    extracted controllers

---
+++ app/controllers/spaces.rb
@@ -0,0 +1,69 @@
+class SpaceBnB < Sinatra::Base
+
+  get '/spaces/new' do
+    send_file 'app/public/spaces/new.html'
+  end
+
+  get '/spaces' do
+    send_file 'app/public/spaces/index.html'
+  end
+
+  post '/spaces/new' do
+    user = current_user
+    user.spaces.create(name: params[:space_name],
+                description: params[:description],
+                price: params[:price],
+                date: [params[:start_date], params[:end_date]])
+    redirect '/spaces'
+  end
+
+  get '/spaces/list' do
+    send_file 'app/public/spaces/list.html'
+  end
+
+  get '/spaces/all' do
+    space = Space.last
+            { id: space.id,
+              name: space.name,
+              description: space.description,
+              price: space.price,
+              available: space.available,
+              date: space.date
+            }.to_json
+  end
+
+  post '/toggle' do
+    space = Space.last
+    space.update(available: false)
+    space.update(date: [])
+    redirect '/spaces/list'
+  end
+
+  post '/space/book' do
+    space = Space.get(params[:book])
+    redirect '/space/book'
+  end
+
+  get '/space/book' do
+    send_file 'app/public/spaces/booking.html'
+  end
+
+
+  get '/spaces/filter-list' do
+    send_file 'app/public/spaces/filter-list.html'
+  end
+
+  post '/spaces/filterdates' do
+    session[:first_date] = params[:first_date]
+    session[:last_date]  = params[:last_date]
+
+    redirect '/spaces/filter-list'
+  end
+
+  get '/spaces/filter' do
+    filter = {first_date: session[:first_date],
+              last_date:  session[:last_date]
+            }.to_json
+  end
+
+end
---
+++ app/controllers/users.rb
@@ -0,0 +1,53 @@
+class SpaceBnB < Sinatra::Base
+
+  get '/welcome' do
+    send_file 'app/public/user/welcome.html'
+  end
+
+  get '/log-in' do
+    send_file 'app/public/user/login.html'
+  end
+
+  post '/log-in' do
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect '/welcome'
+    else
+      redirect '/log-in'
+    end
+  end
+
+  delete '/log-out' do
+    session[:user_id] = nil
+    redirect '/'
+  end
+
+  post '/register' do
+    user = Renter.create(name: params[:name],
+                username: params[:username],
+                email: params[:email],
+                password: params[:password],
+                password_confirmation: params[:password_confirmation])
+
+    user = User.create(name: params[:name],
+                username: params[:username],
+                email: params[:email],
+                password: params[:password],
+                password_confirmation: params[:password_confirmation])
+    if user.save
+      session[:user_id] = user.id
+      redirect '/spaces'
+    else
+      redirect '/'
+    end
+  end
+
+  get '/user/data' do
+    user_data = current_user
+                { id: user_data.id,
+                  name: user_data.name,
+                  username: user_data.username,
+                  email: user_data.email}.to_json
+  end
+end
